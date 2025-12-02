fceeb1d147d8ac2d985f7e0fb36c99cf1eb80ad6
nate <nathancarnahan01@gmail.com>
2025-12-02 14:15:39 -0800

    Added DB connection and basic schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config(); // inits plugin for hiding of db env variable
 
 const app = express();
+const PORT = process.env.PORT;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
