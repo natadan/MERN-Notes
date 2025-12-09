2e9bbae00035a6cb4ab6828259dd828b19bd12e7
nate <nathancarnahan01@gmail.com>
2025-12-08 19:29:11 -0800

    Finished configuring frontend plugins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,12 @@ import toast from "react-hot-toast";
 
 const App = () => {
    return (
-      <div>
-      <button onClick={() => toast.success("congrats")}>Click me</button>
-      <Routes>
-         <Route path = "/" element = {<HomePage />} />
-         <Route path = "/create" element = {<CreatePage />} />
-         <Route path = "/note/:id" element = {<NoteDetailPage />} />
-      </Routes>
-      
+      <div data-theme = "forest">
+         <Routes>
+            <Route path = "/" element = {<HomePage />} />
+            <Route path = "/create" element = {<CreatePage />} />
+            <Route path = "/note/:id" element = {<NoteDetailPage />} />
+         </Routes>
       </div>
    )
 }
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NoteDetailPage() {
   return (
     <div>
-      
+      NoteDetailPage
     </div>
   )
 }
